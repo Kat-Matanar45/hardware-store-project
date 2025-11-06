9c31fe0244c3a5125a6030e0309887ea75987dec
Kat <avchimchik@gmail.com>
2025-11-07 00:42:37 +0500

    Исправлен логотип

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,10 +2,12 @@ import type { ReactElement } from "react";
 
 import NavLinkMenu from "../UI/NavLinkMenu";
 
+import logo from '@assets/images/logo.svg'
+
 const Header = (): ReactElement => {
   return (
     <header className="bg-blue-200 py-8 px-5 flex justify-between shadow-md">
-      <img className="h-6" src="/logo.svg" alt="logo" />
+      <img className="h-6" src={logo} alt="logo" />
       <nav>
         <ul className="flex gap-14">
           <li>
