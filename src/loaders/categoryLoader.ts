cbcc9399c117d678cdb748f7b6978fb660916d13
Kat <avchimchik@gmail.com>
2025-11-07 01:14:14 +0500

    Изменила путь запроса к категории

--- src/loaders/categoryLoader.ts
+++ src/loaders/categoryLoader.ts
@@ -1,7 +1,7 @@
 import type { ICategories } from "../interface/ICategories";
 
 export default async function fetchCategoryLoader(): Promise<ICategories[]> {
-    const res = await fetch(`data.json`);
+    const res = await fetch(`hardware-store-project/data.json`);
     if (!res.ok) {
         throw new Error ('Failed to fetch categories')
     }
