c467fd7e0cd4d3636dda11662ef926caaf30ca55
Kat <avchimchik@gmail.com>
2025-11-07 01:09:31 +0500

    Изображения снова в public

--- src/loaders/productsLoader.ts
+++ src/loaders/productsLoader.ts
@@ -1,7 +1,7 @@
 import type { IProducts } from "../interface/IProducts";
 
 export default async function fetchProductsLoader(): Promise<IProducts[]> {
-    const res = await fetch('data.json');
+    const res = await fetch(`${process.env.PUBLIC_URL}/data.json`);
     if (!res.ok) {
         throw new Error ('Failed to fetch categories')
     }
