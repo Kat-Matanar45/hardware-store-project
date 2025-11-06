b6488f25f54c43722d048dfcd977fe438ad6c2f8
Kat <avchimchik@gmail.com>
2025-11-07 00:50:07 +0500

    Пробуем

--- src/loaders/productsLoader.ts
+++ src/loaders/productsLoader.ts
@@ -1,7 +1,7 @@
 import type { IProducts } from "../interface/IProducts";
 
 export default async function fetchProductsLoader(): Promise<IProducts[]> {
-    const res = await fetch('build/data.json');
+    const res = await fetch('data.json');
     if (!res.ok) {
         throw new Error ('Failed to fetch categories')
     }
